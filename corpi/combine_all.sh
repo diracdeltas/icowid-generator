d2a8eba73172df3ea1b8eb29321c48eb9bf99d5c
yan <yan@mit.edu>
2018-01-24 22:49:15 -0800

    script improvements

--- corpi/combine_all.sh
+++ corpi/combine_all.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 cd "$(dirname "$0")"
+export LANG=C
+
 rm *.txt
 cat text/*.txt >> erowid.txt
-cat ico_text/*.txt >> ico.txt
+cat ico_text/*.txt | tr -cd '\11\12\15\40-\176' | tr '\r' '\n' >> ico.txt
