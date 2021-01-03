208e531c903f653ad59d3a5f49d0df48833956ee
yan <yan@brave.com>
2021-01-03 11:39:22 -0800

    i don't remember what i did here

--- generate_models.py
+++ generate_models.py
@@ -2,12 +2,12 @@ import markovify
 
 with open('corpi/ico.txt') as f:
     text = f.read()
-ico_model = markovify.Text(text, state_size=3).to_json()
+ico_model = markovify.Text(text, state_size=2).to_json()
 with open('ico.json', 'w') as f:
     f.write(ico_model)
 
 with open('corpi/erowid.txt') as f:
     text = f.read()
-erowid_model = markovify.Text(text, state_size=3).to_json()
+erowid_model = markovify.Text(text, state_size=2).to_json()
 with open('erowid.json', 'w') as f:
     f.write(erowid_model)
