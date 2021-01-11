e8a46af03d1ea7f3b69fd6f0916481333a559cc3
yan <yan@brave.com>
2021-01-10 22:54:22 -0800

    add newer tweets

--- corpi/process_json.js
+++ corpi/process_json.js
@@ -1,10 +1,13 @@
 const fs = require('fs')
 
 const tweets = JSON.parse(fs.readFileSync('trump.json'))
-console.log(`parsed ${tweets.length} tweets`)
+const tweets2 = JSON.parse(fs.readFileSync('trump2021.json'))
 
-const text = tweets.map((t) => {
-  return t.text.replace(/(?:https?):\/\/[\n\S]+/g, '').replace(/\.{2,}/g,'.')
+const all = tweets.concat(tweets2)
+console.log(`parsed ${all.length} tweets`)
+
+const text = all.map((t) => {
+  return t.text.replace(/(?:https?):\/\/[\n\S]+/g, '').replace(/\.{2,}/g, '.')
 })
 fs.writeFile('trump.txt', text.join(' '), (err) => {
   if (err) {
